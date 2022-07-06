e30d8e754e504736d134e9085f8fbb899b59cf8c
Diego Becerra <diegofer_1110@hotmail.com>
2022-07-06 10:58:03 -0500

    Add some style to top and bottom text to make memes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,39 +4,51 @@ import { useState } from "react";
 
 export default function Form() {
   const [meme, setMeme] = useState({});
-  const [topText,setTopText]=useState('')
-  const [buttomText,setButtomText]=useState('')
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
 
   function getData() {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
-      .then((r) => r.data.memes.filter((mem) => mem.box_count == 2))
+      .then((r) => r.data.memes.filter((mem) => mem.box_count === 2))
       .then((memFilter) =>
         setMeme(memFilter[Math.floor(Math.random() * memFilter.length)])
       );
   }
 
-  
-
-  function handleTop(e){
-    setTopText(e.target.value)
+  function handleTop(e) {
+    setTopText(e.target.value);
   }
 
-  function handleButtom(e){
-    setButtomText(e.target.value)
+  function handleButtom(e) {
+    setBottomText(e.target.value);
   }
 
   return (
     <div className="container">
       <div className="main--container">
-        <Input placeholder="Top text" className="in--text" onChange={handleTop} />
-        <Input placeholder="Bottom text" className="in--text" onChange={handleButtom} />
+        <Input
+          placeholder="Top text"
+          className="in--text"
+          onChange={handleTop}
+          autoFocus
+        />
+        <Input
+          placeholder="Bottom text"
+          className="in--text"
+          onChange={handleButtom}
+        />
         <button className="send--button" onClick={getData}>
           Make my Meme
         </button>
       </div>
       {Object.entries(meme) !== 0 && (
-        <img src={meme.url} className="meme--img" />
+        <div className="meme">
+          
+          <h2 className="meme--toptext">{topText}</h2>
+          <img src={meme.url} className="meme--img" />
+          <h2 className="meme--bottomtext">{bottomText}</h2>
+        </div>
       )}
     </div>
   );

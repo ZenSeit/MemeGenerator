c683d372b1387c3155f4ae6c8303787cf3c64b84
Diego Becerra <diegofer_1110@hotmail.com>
2022-07-06 11:42:58 -0500

    Refactor code to update form State with single json

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,25 +3,37 @@ import "../Stylesheets/form.css";
 import { useState } from "react";
 
 export default function Form() {
-  const [meme, setMeme] = useState({});
-  const [topText, setTopText] = useState("");
-  const [bottomText, setBottomText] = useState("");
 
-  function getData() {
-    fetch("https://api.imgflip.com/get_memes")
+  const [memeMake,setMemeMake] = useState(
+    {
+      topText:'',
+      bottomText:'',
+      urlImg:''
+    }
+  )
+
+  async function  getData() {
+    await fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((r) => r.data.memes.filter((mem) => mem.box_count === 2))
       .then((memFilter) =>
-        setMeme(memFilter[Math.floor(Math.random() * memFilter.length)])
-      );
+      setMemeMake(
+        {...memeMake,
+          urlImg:memFilter[Math.floor(Math.random() * memFilter.length)].url
+        }
+        )
+      )
   }
 
-  function handleTop(e) {
-    setTopText(e.target.value);
-  }
+  function handleChange(e){
+    const {name,value}=e.target
+    setMemeMake(
+      {
+        ...memeMake,
+        [name]:value,
+      }
+    )
 
-  function handleButtom(e) {
-    setBottomText(e.target.value);
   }
 
   return (
@@ -30,24 +42,26 @@ export default function Form() {
         <Input
           placeholder="Top text"
           className="in--text"
-          onChange={handleTop}
+          onChange={handleChange}
+          name='topText'
           autoFocus
         />
         <Input
           placeholder="Bottom text"
           className="in--text"
-          onChange={handleButtom}
+          onChange={handleChange}
+          name='bottomText'
         />
         <button className="send--button" onClick={getData}>
           Make my Meme
         </button>
       </div>
-      {Object.entries(meme) !== 0 && (
+      {memeMake.urlImg !=='' && (
         <div className="meme">
           
-          <h2 className="meme--toptext">{topText}</h2>
-          <img src={meme.url} className="meme--img" />
-          <h2 className="meme--bottomtext">{bottomText}</h2>
+          <h2 className="meme--toptext">{memeMake.topText}</h2>
+          <img src={memeMake.urlImg} className="meme--img" />
+          <h2 className="meme--bottomtext">{memeMake.bottomText}</h2>
         </div>
       )}
     </div>
